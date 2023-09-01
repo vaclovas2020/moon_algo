e919219fc261694ece082ca4543b9ffc561a6847
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2023-09-01 19:13:38 +0300

    CALCULATE ALL YEAR DATA

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 import math
 
-def calculate_moon_phase(Y,M,D):
+def calculate_moon_phase(Y: int,M: int,D: int) -> float:
     C = E = JD = B = A = F = 0
     if M < 3:
         Y -= 1
@@ -14,8 +14,35 @@ def calculate_moon_phase(Y,M,D):
     S = JD - 2451549.5
     return S % 29.53
 
-Y = int(input('Y'))
-M = int(input('M'))
-D = int(input('D'))
+Y = int(input('Year: '))
 
-print('Days since New Moon:' + math.ceil(calculate_moon_phase(Y,M,D)))
+MoonMonthStart = int(input('Moon month start: '))
+MoonYearStart = int(input('Moon year start: '))
+
+LeapYear = False
+
+if Y % 4 == 0:
+    LeapYear = True
+    if Y % 100 == 0:
+        LeapYear = False
+        if Y % 400 == 0:
+            LeapYear = True
+
+if LeapYear:
+    Days = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+else:
+    Days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+for M in range(1,13):
+    for D in range(1,Days[M-1]+1):
+        print(Y,M,D, sep='-',end=' ')
+        Day=math.ceil(calculate_moon_phase(Y,M,D))
+        if Day == 1 and M != 1:
+            MoonMonthStart += 1
+        if MoonMonthStart > 12:
+            MoonMonthStart = 1
+            MoonYearStart += 1
+        print("Moon year:", MoonYearStart, sep=' ',end=' ')
+        print("Moon month:", MoonMonthStart, sep=' ',end=' ')
+        print("Moon day:",math.ceil(calculate_moon_phase(Y,M,D)), sep=' ')
