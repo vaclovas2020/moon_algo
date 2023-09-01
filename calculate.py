28ba776eb73671f3af3b597f4b3ca0f307332f98
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2023-09-01 21:15:12 +0300

    formating

--- calculate.py
+++ calculate.py
@@ -1,6 +1,7 @@
 import math
 
-def calculate_moon_phase(Y: int,M: int,D: int) -> float:
+
+def calculate_moon_phase(Y: int, M: int, D: int) -> float:
     C = E = JD = B = A = F = 0
     if M < 3:
         Y -= 1
@@ -14,12 +15,13 @@ def calculate_moon_phase(Y: int,M: int,D: int) -> float:
     S = JD - 2451549.5
     return S % 29.53
 
-Year = int(input('Year: '))
 
-HowManyYears = int(input('How many years: '))
+Year = int(input("Year: "))
 
-MoonMonthStart = int(input('Moon month start: '))
-MoonYearStart = int(input('Moon year start: '))
+HowManyYears = int(input("How many years: "))
+
+MoonMonthStart = int(input("Moon month start: "))
+MoonYearStart = int(input("Moon year start: "))
 
 LeapYear = False
 
@@ -27,7 +29,7 @@ Data = []
 
 MoonMonthStart -= 1
 
-DiffYears = [2023,2026,2029,2032]
+DiffYears = [2023, 2026, 2029, 2032]
 
 for Y in range(Year, Year + HowManyYears):
     try:
@@ -41,21 +43,29 @@ for Y in range(Year, Year + HowManyYears):
             LeapYear = False
             if Y % 400 == 0:
                 LeapYear = True
-                
-    if LeapYear:
-        Days = [31,29,31,30,31,30,31,31,30,31,30,31]
-    else:
-        Days = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-    for M in range(1,13):
-        for D in range(1,Days[M-1]+1):
-            Day=math.ceil(calculate_moon_phase(Y,M,D))
+    if LeapYear:
+        Days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else:
+        Days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    for M in range(1, 13):
+        for D in range(1, Days[M - 1] + 1):
+            Day = math.ceil(calculate_moon_phase(Y, M, D))
             if Day == 1:
                 if Done or MoonMonthStart != 2:
                     MoonMonthStart += 1
                 else:
                     Done = True
-                Data.append({'Year':Y,'Month':M,'Day':D,'MoonYear':MoonYearStart,'MoonMonth':MoonMonthStart})
+                Data.append(
+                    {
+                        "Year": Y,
+                        "Month": M,
+                        "Day": D,
+                        "MoonYear": MoonYearStart,
+                        "MoonMonth": MoonMonthStart,
+                    }
+                )
             if MoonMonthStart == 12:
                 MoonMonthStart = 0
                 MoonYearStart += 1
