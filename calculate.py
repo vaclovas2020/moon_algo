f40fa183c10f0ff094256139718b87e9dbc3dd4c
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2023-09-02 07:39:51 +0300

    algo fix

--- calculate.py
+++ calculate.py
@@ -13,7 +13,7 @@ def calculate_moon_phase(Y: int, M: int, D: int) -> float:
     F = int(30.6001 * (M + 1))
     JD = C + D + E + F - 1524.5
     S = JD - 2451549.5
-    return S % 29.53
+    return S % 29.5305882
 
 
 Year = int(input("Year: "))
@@ -51,8 +51,11 @@ for Y in range(Year, Year + HowManyYears):
 
     for M in range(1, 13):
         for D in range(1, Days[M - 1] + 1):
-            Day = math.ceil(calculate_moon_phase(Y, M, D))
-            if Day == 1:
+            Day = calculate_moon_phase(Y, M, D)
+            DayBefore = calculate_moon_phase(Y, M, D - 1)
+            DayAfter = calculate_moon_phase(Y, M, D + 1)
+            DayDayAfter = calculate_moon_phase(Y, M, D + 2)
+            if math.ceil(Day) == 1:
                 if Done or MoonMonthStart != 2:
                     MoonMonthStart += 1
                 else:
@@ -64,9 +67,17 @@ for Y in range(Year, Year + HowManyYears):
                         "Day": D,
                         "MoonYear": MoonYearStart,
                         "MoonMonth": MoonMonthStart,
+                        "MoonDay": Day,
+                        "MoonDayBefore": DayBefore,
+                        "MoonDayAfter": DayAfter,
+                        "DayDayAfter": DayDayAfter,
                     }
                 )
             if MoonMonthStart == 12:
                 MoonMonthStart = 0
                 MoonYearStart += 1
-print(Data)
+
+for i in range(len(Data)):
+    print(
+        f"Sun:{Data[i]['Year']}-{Data[i]['Month']}-{Data[i]['Day']} HC:{Data[i]['MoonYear']}-{Data[i]['MoonMonth']} Day:{Data[i]['MoonDay']} DayBefore:{Data[i]['MoonDayBefore']} DayAfter:{Data[i]['MoonDayAfter']} DayDayAfter:{Data[i]['DayDayAfter']}"
+    )
